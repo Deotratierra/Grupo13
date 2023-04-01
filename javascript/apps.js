7097c31fb02e47b680459e2b69b108a1f84ead3e
jpricart <m.jpricart@gmail.com>
2023-04-01 05:23:47 -0300

    responsive + hamburger y menu
    Header y carousel se hicieron responsive, agregué un hamburger al display movil y un nuevo menu que interactúa con el usuario.
    También ajusté el alto del carousel, y le cambié el tamaño, diseño y color a los indicadores.

--- javascript/apps.js
+++ javascript/apps.js
@@ -1,3 +1,5 @@
+// Evento Sticky del header
+
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   const main = document.querySelector("main");
@@ -12,3 +14,53 @@ window.addEventListener("scroll", function () {
     main.style.paddingTop = 0;
   }
 });
+
+
+// Controlador del botón hamburger
+
+const check = document.querySelector('#check');
+  const menu = document.querySelector('.cont_resp_menu');
+
+  check.addEventListener('click', () => {
+    if (check.checked) {
+      menu.style.display = 'block';
+      setTimeout(() => menu.classList.add('show'), 0);
+    } else {
+      menu.classList.remove('show');
+      setTimeout(() => menu.style.display = 'none', 300);
+    }
+  });
+
+
+  // Controlador que elimina el menú desplegable cuando el usuario toca una opción del <li>
+
+  const menuItems = document.querySelectorAll('.cont_resp_menu li a');
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('show');
+      setTimeout(() => {
+        menu.style.display = 'none';
+        check.checked = false;
+      }, 300);
+    });
+  });
+
+
+// Ocultar el menú si la pantalla tiene un ancho mayor o igual a 640px y el menú está visible
+function hideMenuOnLargeScreens() {
+  const respContMenu = document.querySelector('.cont_resp_menu');
+  if (window.innerWidth >= 640 && respContMenu.classList.contains('show')) {
+    respContMenu.classList.remove('show');
+    setTimeout(() => respContMenu.style.display = 'none', 300);
+    check.checked = false;
+  }
+}
+
+// Ejecutar la función al cargar la página
+hideMenuOnLargeScreens();
+
+// Ejecutar la función cada vez que se cambie el tamaño de la pantalla
+window.addEventListener('resize', hideMenuOnLargeScreens);
+
+
+  

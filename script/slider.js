36e4bcde2dd4f87c3b1b1fc3a5dfe51a83be31d3
Deotratierra <108885561+Deotratierra@users.noreply.github.com>
2023-04-03 13:55:57 -0300

    slider
    se pone js de slider en carpeta script/slider.js

---
+++ script/slider.js
@@ -0,0 +1,37 @@
+      // JavaScript del menú responsive
+        const menuBtn = document.querySelector(".menu-btn");
+        const navigation = document.querySelector(".navigation");
+
+        menuBtn.addEventListener("click", () => {
+            menuBtn.classList.toggle("active");
+            navigation.classList.toggle("active");
+        })
+
+        // Javascript del slider navegador de los videos
+        const btns = document.querySelectorAll(".nav-btn");
+        const slides = document.querySelectorAll(".video-slide");
+        const contents = document.querySelectorAll(".content");
+
+        var sliderNav = function(manual) {
+            btns.forEach((btn) => {
+                btn.classList.remove("active");
+            })
+
+            slides.forEach((slide) => {
+                slide.classList.remove("active");
+            })
+
+            contents.forEach((content) => {
+                content.classList.remove("active");
+            })
+
+            btns[manual].classList.add("active");
+            slides[manual].classList.add("active");
+            contents[manual].classList.add("active");
+        }
+
+        btns.forEach((btn, i) => {
+            btn.addEventListener("click", () => {
+                sliderNav(i);
+            })
+        })
